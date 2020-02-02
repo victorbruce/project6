9080158c77f8ad531120cdc3eb8966fd12615bdb
victorbruce <victorbruce82@gmail.com>
2020-02-02 18:39:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/* Step1: Generating a Map 
+  
+  * Objectives:
+  1. Randomly generate each map.
+  2. Create different types of weapons up to four(4).
+  3. Each weapon should have a name and associated visual.
+  4. Default weapon should carry 10 points for each player.
+  5. Number of players in a game should be two.
+  6. Players must be placed randomly on the map when the game loads
+  6. Players cannot be together.
+
+*/
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# A turn-based board game
+
+This is project 6 of openclassroom frontend development path.
