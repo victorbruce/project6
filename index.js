d450c802156227f146c096f4a8f3d2dbe28ad2dd
victorbruce <victorbruce82@gmail.com>
2020-02-16 20:16:54 +0000

    Add board

--- index.js
+++ index.js
@@ -11,3 +11,45 @@
 
 */
 
+// weapons
+const weapons = [
+  { name: "potatoes", image: "", default: false, points: 20 },
+  { name: "tomatoes", image: "", default: true, points: 10 },
+  { name: "garden-eggs", image: "", default: false, points: 15 },
+  { name: "pepper", image: "", default: false, points: 10 }
+];
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+const ROW = 15;
+const COL = 15;
+const VACANT = "WHITE";
+const SQ = 40;
+
+function drawSquare(x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect = (x * SQ, y * SQ, SQ, SQ);
+  ctx.strokeStyle = "black";
+  ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
+}
+
+// create board
+let board = [];
+for (let r = 0; r < ROW; r++) {
+  board[r] = [];
+
+  for (let c = 0; c < COL; c++) {
+    board[r][c] = "VACANT";
+  }
+}
+
+// Draw board
+function drawBoard() {
+  for (let r = 0; r < ROW; r++) {
+    for (let c = 0; c < COL; c++) {
+      drawSquare(c, r, board[r][c]);
+    }
+  }
+}
+
+drawBoard();
